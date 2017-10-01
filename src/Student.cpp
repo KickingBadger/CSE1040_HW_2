98b38c8f2d427207aa6ae99a314afde0d67ab4d1
Justin <justin_hinton@ymail.com>
2017-10-01 02:16:03 -0500

    Hopfully final reorganization, got make working. Can actually start now.

--- src/Student.cpp
+++ src/Student.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 #include "Student.h"
 
-Student::Student() {
+Student::Student(int newId, string newName, Standing newstanding) {
 	Id = 0;
 	Name = "";
 }
--- src/main.cpp
+++ src/main.cpp
@@ -19,7 +19,5 @@ using namespace std;
 
 int main () {
 
-	Student Justin;
-	
 	return 0;
 };
